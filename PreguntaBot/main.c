fde509d2f54a1c7a54a32f9892dfaf77d866dffc
Fernando Mazzon <fer662@gmail.com>
2014-05-04 20:03:09 -0300

    Initial Commit

---
+++ PreguntaBot/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  PreguntaBot
+//
+//  Created by Fernando Mazzon on 5/4/14.
+//  Copyright (c) 2014 Fer662. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
